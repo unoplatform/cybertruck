caff17657fad05ef6b6c77b73fcf6f4e12918153
Jerome Laban <jerome.laban@nventive.com>
2019-12-02 21:59:48 -0500

    Use proper name

---
+++ Cybertruck.Wasm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Uno.Foundation;
+using Windows.UI.Xaml;
+
+namespace CyberTruck.Wasm
+{
+	public class Program
+	{
+		private static App _app;
+
+		static int Main(string[] args)
+		{
+			WebAssemblyRuntime.InvokeJS("Uno.UI.Demo.Analytics.reportPageView('main');");
+
+			Windows.UI.Xaml.Application.Start(_ => _app = new App());
+
+			return 0;
+		}
+	}
+}
